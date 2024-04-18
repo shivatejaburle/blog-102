ccf86189cb17a8be58ed22ad6b509e8c403ec4cc
Shivateja Burle <shivateja.idizital@gmail.com>
2024-04-18 11:59:56 +0530

    django-allauth
    For User Management

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
+    path('accounts/', include('allauth.urls')),
 ]

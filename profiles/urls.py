dbbfdcafb58ebbeffa5db536e87b5ffa4fab1761
Shivateja Burle <shivateja.idizital@gmail.com>
2024-05-07 12:49:57 +0530

    Profile App
    Update User Profile, Upload, Change or Remove Profile Picture.

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'profiles'
 
 urlpatterns = [
-    path("detail/<int:pk>", views.UserProfile.as_view(), name = 'profile_detail')
+    path("detail/<int:pk>", views.UserProfile.as_view(), name = 'profile_detail'),
+    path("update/<int:pk>", views.UpdateUserProfile.as_view(), name = 'profile_update'),
+    # Update Profile Picture
+    path("update/picture/<int:pk>", views.UpdateProfilePicture, name = 'picture_update'),
+    # Delete Profile Picture
+    path("delete/picture/<int:pk>", views.DeleteProfilePicture, name = 'picture_delete')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, UpdateView
 from profiles.models import Profile
 from django.contrib.auth.models import User
+import os
 
 # User Profile Detail View
 class UserProfile(DetailView):
@@ -9,9 +11,39 @@ class UserProfile(DetailView):
     # context_object_name = 'profile'
     # template_name = 'profiles/profile_detail.html'
     
-
     # def get_queryset(self):
     #     userId = User.objects.filter(id = self.kwargs['pk'])
     #     print(userId[0].profiles.id)
     #     queryset = self.model.objects.filter(id = userId[0].profiles.id)
     #     return queryset
+
+# Update User Profile
+class UpdateUserProfile(UpdateView):
+    model = Profile
+    fields = ['first_name','last_name', 'mobile', 'address']
+
+    def get_success_url(self):
+        return reverse_lazy('profiles:profile_detail', kwargs={'pk' : self.kwargs['pk']})
+
+# Update Profile Picture
+def UpdateProfilePicture(request, pk):
+    profile = Profile.objects.get(id=pk)
+    if request.method == "POST":
+        if len(request.FILES) != 0:
+            if profile.image:
+                os.remove(profile.image.path)
+            profile.image = request.FILES['image_document']
+        profile.save()
+        success_url = '/profile/detail/' + str(profile.id)
+        return redirect(success_url)
+    context = {'profile':profile}
+    return render(request, 'profiles/picture_form.html', context)
+
+# Remove Profile Picture
+def DeleteProfilePicture(request, pk):
+    profile = Profile.objects.get(id=pk)
+    os.remove(profile.image.path)
+    profile.image=""
+    profile.save()
+    success_url = '/profile/detail/' + str(profile.id)
+    return redirect(success_url)
